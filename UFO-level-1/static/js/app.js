176bd95b5548627cf0de4f3f6e428ac5b3891179
mneralla <maanasaneralla@gmail.com>
2020-10-22 00:06:50 -0500

    JS Update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,12 +1,10 @@
 // Pulls data from data.js
 var tableData = data;
 
-//Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
-
-//get a reference for the table body
+//Gets reference for the table body
 var tbody = d3.select("tbody");
 
-//Loop through each object in dataset and then loop through the keys and values and add the values to the cells
+//Loops through each object in the dataset, then will loop through keys and values to add values in cells
 tableData.forEach((ufoData) => {
     var row = tbody.append("tr");
     Object.entries(ufoData).forEach(([key,value]) =>{
@@ -15,8 +13,7 @@ tableData.forEach((ufoData) => {
     });
 });
 
-//Make sure you have a column for `date/time`, `city`, `state`, `country`, `shape`, and `comment` at the very least.
-//Use a date form in your HTML document and write JavaScript code that will listen for events and search through the `date/time` column to find rows that match user input.
+//Listens for events and searches the `date/time` column to find rows matching user input.
 var button = d3.select("#filter-btn");
 var form = d3.select("#form");
 
@@ -24,7 +21,7 @@ button.on("click",runEnter);
 form.on("submit",runEnter);
 
 function runEnter(){
-    //Prevent page from refreshing
+    //Prevents page from refreshing
     d3.event.preventDefault();
     
     var dateEl = d3.select("#datetime");
@@ -33,7 +30,7 @@ function runEnter(){
     var filterDate = tableData.filter(date => date.datetime === inputdate);
     tbody.html("");
     
-    //Create new table based on filtered data
+    //Creates a table for filtered data
     filterDate.forEach((date) => {
     var row = tbody.append("tr");
     Object.entries(date).forEach(([key,value]) =>{
